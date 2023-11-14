ab1a6df7b61947167c4d16bba349927a0b95361a
Erwin van Lun <erwin@vanlun.com>
2023-11-14 13:43:23 +0100

    stylesheet pink bluegrey to begin with

--- libs/yes/src/lib/shell/menu/menu.component.ts
+++ libs/yes/src/lib/shell/menu/menu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'yes-menu',
   templateUrl: 'menu.component.html',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule]
+  imports: [MatButtonModule, MatMenuModule, MatIconModule]
 })
 export class MenuComponent {
 }
