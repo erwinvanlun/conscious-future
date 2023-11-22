77c321276d786c5d612ab89f59b351eb4881c2c6
Erwin van Lun <erwin@vanlun.com>
2023-11-22 17:28:45 +0100

    typing environment

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,6 +1,7 @@
 import * as GitBranch from 'tools/git-branch.const.js';
+import { Environment } from '@biodanza/yes';
 
-export const environment = {
-  projectId: 'blab',
+export const environment: Environment = {
+  production: false,
   branchName: GitBranch.GIT_BRANCH
 };
--- libs/yes/src/index.ts
+++ libs/yes/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/yes.module';
 export * from './lib/environment/environment';
+export * from './lib/environment/environment.type';
 
